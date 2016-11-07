1b335f5733dbcd5fd25e2caacc84bd1a5d426c86
Alkesh <alkeshsrivastava11@gmail.com>
2016-11-07 23:50:23 +0530

    A lot of changes

--- server.js
+++ server.js
@@ -8,17 +8,6 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-app.get('/article-one',function(req,res){
-    res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
-});
-
-app.get('/article-two',function(req,res){
-    res.sendFile(path.join(__dirname,'ui','article-two.html'));
-});
-
-app.get('/article-three',function(req,res){
-    res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
-});
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
@@ -28,12 +17,15 @@ app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
-app.get('/ui/225H.jpg', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', '225H.jpg'));
+app.get('/ui/header.png', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'header.png'));
 });
 
 app.get('/ui/header.png', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'header.png'));
+  res.sendFile(path.join(__dirname, 'ui', 'develop.png'));
+});
+app.get('/ui/header.png', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'design.png'));
 });
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
