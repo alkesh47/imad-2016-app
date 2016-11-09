a1474723d4f4adfff7d8d9275e43ca6e688c4d5b
Alkesh <alkeshsrivastava11@gmail.com>
2016-11-09 19:35:22 +0530

    Added a counter end point

--- server.js
+++ server.js
@@ -79,6 +79,16 @@ app.get('/profile', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'profile.html'));
 });
 
+app.get('/blog', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'blog.html'));
+});
+
+var counter = 0;
+app.get('/counter', function (req, res) {                       // counter end-point
+  counter = counter + 1;
+  res.send(counter.toString())
+});
+
 app.get('/:NameOfArticle', function (req, res) {
   var NameOfArticle=req.params.NameOfArticle;        //It is used to extract the name of article into a variable so that we can use to index the correct article.
   res.send(CreateTemplate(articles[NameOfArticle]));
@@ -86,6 +96,10 @@ app.get('/:NameOfArticle', function (req, res) {
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
+});
+
+app.get('/ui/main.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
 
 app.get('/ui/madi.png', function (req, res) {
--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,20 @@
-console.log('Loaded!');
+var button= document.getElementById('counter');
+var counter=0;
+
+button.onclick = function(){
+  // Create a request object
+  var request = new XMLHttpRequest();
+  request.onreadystatechange = function(){
+    if (request.readystatechange === XMLHttpRequest.DONE) {
+      if (request.status === 200) {
+        var counter = request.responseText;
+        var span = document.getElementById('count');
+        span.innerHTML = count.toString();
+      }
+    }
+  }
+
+  // Make a request object
+  request.open('GET', 'http://localhost:8080/counter', true)
+  request.send(null)
+};
