ca3fe3022676dd158f6984df09c0d9c1743f2f79
Alkesh <alkeshsrivastava11@gmail.com>
2016-11-12 21:50:02 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -24,7 +24,7 @@ submit.onclick = function(){
 
   var nameInput = document.getElementById('name');
   var name = nameInput.value;
-  request.open('GET', 'http://cloud.imad.hasura.io/alkesh47/:NameOfArticle/submit-name?name='+name, true);
+  request.open('GET', 'http://alkesh47.cloud.imad.hasura.io/:NameOfArticle/submit-name?name='+name, true);
   request.send(null);
 
 };
