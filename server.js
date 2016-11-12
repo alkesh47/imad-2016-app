e02fa88a232dfcd99454a3431340fb7056c19d39
Alkesh <alkeshsrivastava11@gmail.com>
2016-11-13 01:40:33 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ var path = require('path');
 var Pool = require('pg').Pool;
 var crypto = require('crypto');
 var bodyParser = require('body-parser');
+var session = require("express-session");
 
 var config = {
   host: 'db.imad.hasura-app.io',
@@ -17,6 +18,10 @@ var config = {
 var app = express();
 app.use(morgan('combined'));
 app.use(bodyParser.json());
+app.use(session({
+    secret: "someRandomSecretValue",
+    cookie: {maxAge: 1000*60*60*24*30}
+}));
 
 var articles={      // This variable holds all the common part of all the articles
   'articleOne' :{
@@ -198,6 +203,9 @@ app.post('/login', function(req,res){
             var salt = dbString.split('$')[2];
             var hashedPassword = hash(password,salt);
             if(hashedPassword === dbString){
+                //set the session
+                req.session.auth = {userId: result.rows[0].id};
+                
                 res.send("Correct Credentials");    
             }
             else{
@@ -207,6 +215,16 @@ app.post('/login', function(req,res){
         }
     }
   });
+});
+
+app.get('/check-login', functon (req,res){
+    if(req.session && req.session.auth && req.session.auth.userId){
+        res.send('You are logged in '+req.session.auth.userId.toString());
+    }
+    
+    else{
+        res.send('You are not logged in');
+    }
 });
 
 app.get('/articles/:NameOfArticle', function (req, res) {
